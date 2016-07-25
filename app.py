39d6d8d7450ad7c9074a24be4f108ce0a95f1769
Cal Rueb <crueb@amfam.com>
2016-07-25 10:43:44 -0500

    Makes/prompts config file if one doesn't exist

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import smtplib
- 
-# Read config file
-config = open('.config.txt', 'r')
-email = config.readline().rstrip()
-password = config.readline().rstrip()
+import os.path
+
+# Check if config exists
+if(not os.path.isfile('.config.txt')):
+    email = raw_input("Enter your gmail account: ")
+    password = raw_input("Enter your password: ")
+    config = open('.config.txt', 'w')
+    config.write(email+'\n'+password)
+    config.close()
+else:
+    config = open('.config.txt', 'r')
+    email = config.readline().rstrip()
+    password = config.readline().rstrip()
+    config.close()
 
 # Send email
 server = smtplib.SMTP('smtp.gmail.com', 587)
