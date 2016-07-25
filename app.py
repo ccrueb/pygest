88553db0c4471eebb5ed4703b2b073d0e7612cdb
Cal Rueb <crueb@amfam.com>
2016-07-25 10:28:19 -0500

    Read and send from config file

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import smtplib
  
+# Read config file
+config = open('.config.txt', 'r')
+email = config.readline().rstrip()
+password = config.readline().rstrip()
+
+# Send email
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login("email", "pass")
- 
+server.login(email, password)
 msg = "YOUR MESSAGE!"
-server.sendmail("email", "email", msg)
+server.sendmail(email, email, msg)
 server.quit()
